c14caf1e3cb1cc744a9774c92d92824dab7f5f8e
Ghusde <65841565+Ghusde@users.noreply.github.com>
2022-11-27 21:30:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Designer", "Freelancer", "Blogger"],
+        strings: ["Graphic Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Designer", "Freelancer", "Blogger"],
+        strings: ["Graphic Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
